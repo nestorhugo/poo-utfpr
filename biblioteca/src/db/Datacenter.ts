a54836245f4275ffa874999360ec76816eb09f51
nestorhugo <nestorhugo@proton.me>
2024-04-07 18:34:35 -0300

    add Student e Library no Datacenter

--- biblioteca/src/db/Datacenter.ts
+++ biblioteca/src/db/Datacenter.ts
@@ -1,9 +1,15 @@
 import Book from "../model/Book";
+import Library from "../model/Library";
+import Student from "../model/Student";
 //lei da responsabilidade unica.
 //cabe ao Datacenter guardar, acessar e manipular os dados de
 // FoodProduct e CleaningProduct
 export default class Datacenter {
   public book: Book[] = [];
+  public library: Library[] = [];
+  public student: Student[] = [];
+
+  // 👇👇Books!
 
   public addNewBook(food: Book): void {
     this.book.push(food);
@@ -15,5 +21,31 @@ export default class Datacenter {
 
   public getBookSize(): number {
     return this.book.length;
+  }
+
+  // 👇👇Library!
+  public addNewLibrary(library: Library): void {
+    this.library.push(library);
+  }
+
+  public removeLibrary(id: number): void {
+    this.library.splice(id, 1);
+  }
+
+  public getLibrarySize(): number {
+    return this.library.length;
+  }
+
+  // 👇👇Students!
+  public addNewStudent(student: Student): void {
+    this.student.push(student);
+  }
+
+  public removeStudent(id: number): void {
+    this.student.splice(id, 1);
+  }
+
+  public getStudentSize(): number {
+    return this.student.length;
   }
 }
